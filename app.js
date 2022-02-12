ecc3168f736d8570b511e5bf3a03c11a0c2a09e5
ndenisj <denis.john.ndarake@gmail.com>
2022-02-12 23:20:13 +0100

    moved to intro folder

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const http = require('http')
-const fs = require('fs')
-
-http.createServer((req, res)=>{
-    // const text = fs.readFileSync('./content/big.txt', 'utf-8')
-    // res.end(text)
-
-    const fileStream = fs.createReadStream('./content/big.txt', 'utf-8')
-    fileStream.on('open', ()=>{
-        fileStream.pipe(res) // sending the data in chucks
-    })
-    fileStream.on('error', (err)=>{
-        res.end(err)
-    })
-}).listen(5000)
+console.log('Express');
